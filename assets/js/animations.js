77f679c1ab08c199baa6f354fe05b6a3f73249ab
Joanna Wolska <asia@MacBook-Pro-3.local>
2018-03-04 22:56:01 +0100

    Poprawiono skrypt z animacja, uzywamy zmiennych

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,15 +1,11 @@
 $( document ).ready(function() {
 
-    $('#o-nas-scroll').click(function () {
-        scrollTop: $('#o-nas-scroll').offset().top
-    });
-
     $(window).scroll(function () {
         var scrollfromtop = $(this).scrollTop();
 
 
 
-        if($(this).scrollTop() > $('.person-row').offset().top - $(window).height()){
+        if(scrollfromtop > $('.person-row').offset().top - $(window).height()){
             $('.person').eq(0).addClass('slide-in-right');
             $('.person').eq(1).addClass('slide-in-left');
         }
