e6069b8f4a0f784bd78abacd4f986771be5b0dc2
Piotrek Cieśla <piotrek.ciesla69@gmail.com>
2018-03-07 21:31:28 +0100

    skill bar

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -8,6 +8,20 @@ $( document ).ready(function() {
         if(scrollfromtop > $('.person-row').offset().top - $(window).height()){
             $('.person').eq(0).addClass('slide-in-right');
             $('.person').eq(1).addClass('slide-in-left');
+
+
+
+            $('.person').eq(0).children(".skill").each(function (e) {
+                var skill_barr = $( this ).children(".skill-bar");
+                var skill_value = skill_barr.attr( 'skill' );
+                skill_barr.css({"width": ""+ skill_value +"%" })
+            });
+
+            $('.person').eq(1).children(".skill").each(function (e) {
+                var skill_barr = $( this ).children(".skill-bar");
+                var skill_value = skill_barr.attr( 'skill' );
+                skill_barr.css({"width": ""+ skill_value +"%" })
+            });
         }
 
     });
