f4c8234d9dc01616ab86b8ec9a17ffaed764d53f
Piotrek Cieśla <piotrek.ciesla69@gmail.com>
2018-03-16 18:04:10 +0100

    new header animation

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -5,20 +5,20 @@ $( document ).ready(function() {
 
 
 
-        if(scrollfromtop > $('.person-row').offset().top - $(window).height()){
+        if(scrollfromtop > $('.person-row').offset().top - $(window).height() + $(window).height()*0.40){
             $('.person').eq(0).addClass('slide-in-right');
             $('.person').eq(1).addClass('slide-in-left');
 
 
 
-            $('.person').eq(0).children(".skill").each(function (e) {
-                var skill_barr = $( this ).children(".skill-bar");
+            $('.person').eq(0).children(".skill-container").each(function (e) {
+                var skill_barr = $( this ).children(".skill").children(".skill-bar");
                 var skill_value = skill_barr.attr( 'skill' );
                 skill_barr.css({"width": ""+ skill_value +"%" })
             });
 
-            $('.person').eq(1).children(".skill").each(function (e) {
-                var skill_barr = $( this ).children(".skill-bar");
+            $('.person').eq(1).children(".skill-container").each(function (e) {
+                var skill_barr = $( this ).children(".skill").children(".skill-bar");
                 var skill_value = skill_barr.attr( 'skill' );
                 skill_barr.css({"width": ""+ skill_value +"%" })
             });
